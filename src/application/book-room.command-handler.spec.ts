7195fd4831632f8a6fc32116721c69e68af373d7
tλeUniC <318014+theUniC@users.noreply.github.com>
2023-10-05 23:03:24 +0200

    Make test pass

--- src/application/book-room.command-handler.spec.ts
+++ src/application/book-room.command-handler.spec.ts
@@ -1,8 +1,27 @@
 import { expect } from '@jest/globals';
+import { isAfter, subDays } from 'date-fns';
 import { InvalidDateRangeProvided } from '../domainmodel/InvalidDateRangeProvided';
+import { BookRoomCommand } from './book-room.command';
+
+class BookRoomCommandHandler {
+  async execute(command: BookRoomCommand) {
+    if (isAfter(command.from, command.to)) {
+      throw new InvalidDateRangeProvided();
+    }
+  }
+}
 
 describe('BookRoom', () => {
-  it('should throw an exception when departure date is before arrival date', () => {
+  it('should throw an exception when departure date is before arrival date', async () => {
+    const commandHandler = new BookRoomCommandHandler();
+    const command = new BookRoomCommand(
+      'test',
+      'test',
+      new Date(),
+      subDays(new Date(), 2),
+    );
+
+    const result = commandHandler.execute(command);
     expect(result).rejects.toThrow(InvalidDateRangeProvided);
   });
 });
