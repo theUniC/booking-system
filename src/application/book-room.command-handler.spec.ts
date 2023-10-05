dc0b8b334cd5b95654febb4654b13d2aad109c78
tλeUniC <318014+theUniC@users.noreply.github.com>
2023-10-05 23:20:40 +0200

    Failing test – Room already booked

--- src/application/book-room.command-handler.spec.ts
+++ src/application/book-room.command-handler.spec.ts
@@ -1,8 +1,9 @@
 import { expect } from '@jest/globals';
-import { subDays } from 'date-fns';
+import { addDays, subDays } from 'date-fns';
 import { InvalidDateRangeProvided } from '../domainmodel/InvalidDateRangeProvided';
 import { BookRoomCommand } from './book-room.command';
 import { BookRoomCommandHandler } from './book-room.command-handler';
+import { RoomAlreadyBooked } from '../domainmodel/RoomAlreadyBooked';
 
 describe('BookRoom', () => {
   it('should throw an exception when departure date is before arrival date', async () => {
@@ -16,5 +17,30 @@ describe('BookRoom', () => {
 
     const result = commandHandler.execute(command);
     expect(result).rejects.toThrow(InvalidDateRangeProvided);
+  });
+
+  it('should throw an exception when the room is already booked', async () => {
+    const commandHandler = new BookRoomCommandHandler();
+    const departureDate = new Date();
+
+    await commandHandler.execute(
+      new BookRoomCommand(
+        'test',
+        'test',
+        departureDate,
+        addDays(departureDate, 2),
+      ),
+    );
+
+    const result = commandHandler.execute(
+      new BookRoomCommand(
+        'test1',
+        'test',
+        departureDate,
+        subDays(departureDate, 2),
+      ),
+    );
+
+    expect(result).rejects.toThrow(RoomAlreadyBooked);
   });
 });
